74d316af336f0501826c29d54b3ff050e5deddff
Pascal Martineau <pascal@lewebsimple.ca>
2021-03-01 07:54:22 -0500

    Add explanations

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router";
 import generatedRoutes from "pages-generated";
-import { setupLayouts } from "layouts-generated";
 import App from './App.vue'
 
 const app = createApp(App);
 
+// With vite-plugin-vue-layouts
+import { setupLayouts } from "layouts-generated";
 const routes = setupLayouts(generatedRoutes);
 const router = createRouter({ history: createWebHistory(), routes });
+
+// Without vite-plugin-vue-layouts
+//const router = createRouter({ history: createWebHistory(), routes: generatedRoutes  });
+
 app.use(router);
 
 app.mount("#app");
